842848b9b9e4be40c7509f429de56f5fd7b6d7e8
heeseo333 <heeseo310@gmail.com>
2024-06-05 19:32:05 +0900

    main.js import

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import P_WinningResult from "./pages/P_WinningResult.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import { RecoilRoot } from "recoil";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { path: "/signup", element: <Signup /> },
-      { path: "/login", element: <Login /> },
+      // { path: "/signup", element: <Signup /> },
+      // { path: "/login", element: <Login /> },
+      { path: "/winResult", element: <P_WinningResult /> },
     ],
   },
 ]);
---
+++ src/pages/P_WinningResult.jsx
@@ -0,0 +1,13 @@
+import "../css/P_WinningResult.css";
+import React from "react";
+const P_WinningResult = () => {
+  return (
+    <>
+      <div className="winResult">
+        <p>dfdf</p>
+      </div>
+    </>
+  );
+};
+
+export default P_WinningResult;
