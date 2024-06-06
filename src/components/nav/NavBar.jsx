254c1bd99c24cb869a97db8a0e1a524bb9d0e071
parkhyunseo <dazzle1999@naver.com>
2024-06-06 18:49:49 +0900

    [Refactor]: Refactored li tag to Link tag

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -8,19 +8,24 @@ const NavBar = () => {
         <div className="nav_wrap">
           <nav className="nav">
             <ul className="top-menu">
-              <li className="nav-list">
+              <Link to="/lotto" className="nav-list">
                 로또구매
-                {/* <ul className="dropdown">
-                  <li>옵션 1</li>
-                  <li>옵션 2</li>
-                  <li>옵션 3</li>
-                </ul> */}
-              </li>
-              <li className="nav-list">연금복권구매</li>
-              <li className="nav-list">당첨결과</li>
-              <li className="nav-list">판매점</li>
-              <li className="nav-list">이벤트</li>
-              <li className="nav-list">통계</li>
+              </Link>
+              <Link to="/" className="nav-list">
+                연금복권구매
+              </Link>
+              <Link to="/" className="nav-list">
+                당첨결과
+              </Link>
+              <Link to="/" className="nav-list">
+                판매점
+              </Link>
+              <Link to="/" className="nav-list">
+                이벤트
+              </Link>
+              <Link to="/" className="nav-list">
+                통계
+              </Link>
             </ul>
           </nav>
         </div>
