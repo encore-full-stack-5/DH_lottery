1d189fcb2dfff378a681aebd47a4e2eef2e004f4
giraffenostrils <dreamthre@gamil.com>
2024-06-10 09:36:06 +0900

    re push

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import App from "./App.jsx";
 import "./index.css";
-import P_WinningResult from "./pages/P_WinningResult.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./pages/Layout.jsx";
-import Payment from "./pages/Payment.jsx";
 import Login from "./pages/login/Login.jsx";
 import SignUp from "./pages/singUp/SingUp.jsx";
-import { RecoilRoot } from "recoil";
 import JoinFormAgree from "./pages/singUp/JoinFormAgree.jsx";
-=======
 import { RecoilRoot } from "recoil";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import P_Buying from "./pages/P_Buying.jsx";
-
 import P_Buying from "./pages/p_buying/P_Buying.jsx";
-
 import P_WinningResult from "./pages/P_WinningResult.jsx";
 import Layout from "./pages/Layout.jsx";
 import Withdraw from "./pages/withdraw/Withdraw.jsx";
@@ -30,32 +19,25 @@ import { LottoResultPage } from "./pages/lotto/LottoResultPage.jsx";
 import P_MyWin720 from "./pages/p_lottery/P_MyWin720.jsx";
 import HorizontalBar from "./components/lotto-analytics/HoryzontalBar.jsx";
 import PieChart from "./components/lotto-analytics/PieChart.jsx";
->>>>>>> 3eac28c8ae8095b7873eec898829f9f60becf32e
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      // { path: "/signup", element: <Signup /> },
-      // { path: "/login", element: <Login /> },
       { path: "/winResult", element: <P_WinningResult /> },
       { path: "/buying720", element: <P_Buying /> },
       { path: "/payment", element: <Payment /> },
-<<<<<<< HEAD
       { path: "/login", element: <Login /> },
       { path: "/singUp", element: <SignUp /> },
       { path: "/joinFormAgree", element: <JoinFormAgree /> },
-=======
       { path: "/lotto", element: <Lotto /> },
       { path: "/lotto_result", element: <LottoResultPage /> },
-
       { path: "/withdraw", element: <Withdraw /> },
       { path: "/myWin720", element: <P_MyWin720 /> },
       { path: "/transaction", element: <Transaction /> },
       { path: "/horizontal", element: <HorizontalBar /> },
       { path: "/pie", element: <PieChart /> },
->>>>>>> 3eac28c8ae8095b7873eec898829f9f60becf32e
     ],
   },
 ]);
