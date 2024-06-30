4c594abbb1befb8605da3aa0b4cfa830a545a711
heeseo333 <heeseo310@gmail.com>
2024-07-01 02:05:21 +0900

    [Refactor] Pension-draw BaseURL 수정

--- src/config/network.js
+++ src/config/network.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = async (url, method, body) => {
-  axios.defaults.baseURL = "http://localhost:8080";
+  axios.defaults.baseURL = "http://34.173.193.169:31000";
   // const token = JSON.parse(localStorage.getItem("token"));
   
   const res = await axios({
