73464e3572668046f706dd9ff9d0be83d8b8cd0c
BoonDoPark <BoonDoPark>
2024-07-01 10:23:18 +0900

    [FIX] a -> Link

--- src/components/lotto-analytics/LottoAnalytics.jsx
+++ src/components/lotto-analytics/LottoAnalytics.jsx
@@ -22,9 +22,9 @@ const LottoAnalytics = () => {
                 >
                   기간별로 당첨번호 출현횟수, 빈도수 확인
                 </p>
-                <a href="/horizontal" className="details-button">
+                <Link to="/lotto_analytics/vertical" className="details-button">
                   자세히 보기
-                </a>
+                </Link>
               </div>
               <div className="card-image">
                 <img src="../../../public/bg_list_stat1.png" alt="저런" />
@@ -44,9 +44,9 @@ const LottoAnalytics = () => {
                 >
                   회차별로 당첨번호 색상 확인
                 </p>
-                <a href="/pie" className="details-button">
+                <Link to="/lotto_analytics/pie" className="details-button">
                   자세히 보기
-                </a>
+                </Link>
               </div>
               <div className="card-image">
                 <img src="../../../public/bg_list_stat2.png" alt="저런" />
@@ -66,9 +66,9 @@ const LottoAnalytics = () => {
                 >
                   구간대별 당첨번호 출현빈도 확인
                 </p>
-                <a href="/vertical" className="details-button">
+                <Link to="/lotto_analytics/horizontal" className="details-button">
                   자세히 보기
-                </a>
+                </Link>
               </div>
               <div className="card-image">
                 <img src="../../../public/bg_list_stat3.png" alt="저런" />
@@ -88,9 +88,9 @@ const LottoAnalytics = () => {
                 >
                   회차별로 홀짝 비율 및 번호합 확인
                 </p>
-                <a href="/even_odd" className="details-button">
+                <Link to="/lotto_analytics/even_odd" className="details-button">
                   자세히 보기
-                </a>
+                </Link>
               </div>
               <div className="card-image">
                 <img src="../../../public/bg_list_stat4.png" alt="저런" />
